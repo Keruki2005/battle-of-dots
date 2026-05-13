e79da6cf510cf4755ec261623694a64300bde473
Keruki2005 <mario.russo3030@gmail.com>
2026-05-13 12:08:40 +0200

    Update bod.py

--- bod.py
+++ bod.py
@@ -1,13 +1,13 @@
-from src import wod_server, wod_client
+from src import bod_server, bod_client
 
 def main():
     choice = input("server (1) or client (2)?\n> ")
 
     if choice == "1":
-        wod_server.main()
+        bod_server.main()
     
     elif choice == "2":
-        wod_client.main()
+        bod_client.main()
 
     else:
         print("invalid choice.")

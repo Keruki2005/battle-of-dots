0bac739f8cc481edc549a16a1f251418bb96b2b0
Keruki2005 <mario.russo3030@gmail.com>
2026-05-27 09:33:29 +0200

    Update and rename bode.py to bod.py

--- bod.py
+++ bod.py
@@ -1,16 +1,16 @@
-from src import bode_server, bode_client, bode_map_editor
+from src import bod_server, bod_client, bod_map_editor
 
 def main():
     choice = input("server (1), client (2), or map editor (3)?\n> ")
 
     if choice == "1":
-        bode_server.main()
+        bod_server.main()
     
     elif choice == "2":
-        bode_client.main()
+        bod_client.main()
 
     elif choice == "3":
-        bode_map_editor.main()
+        bod_map_editor.main()
 
     else:
         print("invalid choice.")

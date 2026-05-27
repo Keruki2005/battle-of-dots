b79cdd0552e2060cc3f7b2b9338777a2de135611
Keruki2005 <mario.russo3030@gmail.com>
2026-05-27 09:30:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="battle-of-dots-experimental",
+    name="battle-of-dots",
     version="0.0.7",
     description="Battle of Dots is a LAN RTS war game, a simple barebones war simulation built in python with pygame. Original credit goes to",
     author="Keruki2005",
